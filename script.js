6c63f8867bbbd3440e1c9a62bdf10120d53a7ae7
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-11-29 14:30:12 +0100

    Calculator works nicely, next feature will be changing module to procent and custom error alert

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const value = document.querySelector(".button");
 const newValueBtn = document.querySelectorAll(".number");
 const resetBtn = document.querySelector(".reset");
+const deletePrevNumBtn = document.querySelector(".deletePrevNum");
 const resultBtn = document.querySelector(".result");
 const mathOperation = document.querySelector(".value");
-let result = 0;
 
 const handleOperationChange = (e) => {
   const newValue = e.target.closest(".number");
+  const length = mathOperation.value.length;
   if (
     newValue.value == "+" ||
     newValue.value == "-" ||
     newValue.value == "*" ||
-    newValue.value == "/"
+    newValue.value == "/" ||
+    newValue.value == "%"
   ) {
     if (
       mathOperation.value.slice(-3) !== " + " &&
       mathOperation.value.slice(-3) !== " - " &&
       mathOperation.value.slice(-3) !== " * " &&
-      mathOperation.value.slice(-3) !== " / "
-      // ZA DUZO KROPEK
+      mathOperation.value.slice(-3) !== " / " &&
+      mathOperation.value.slice(-3) !== " % " &&
+      length !== 0
     ) {
       mathOperation.value += ` ${newValue.textContent} `;
     }
@@ -31,15 +34,35 @@ const handleReset = () => {
   mathOperation.value = "";
 };
 
-const handleCount = () => {
-  result = eval(mathOperation.value);
-  mathOperation.value = result;
-  console.log(result);
+const handleDeletePrevNum = () => {
+  const operators = mathOperation.value.slice(-3);
+  if (
+    mathOperation.value.length >= 3 &&
+    (operators == " + " ||
+      operators == " - " ||
+      operators == " / " ||
+      operators == " * " ||
+      operators == " % ")
+  ) {
+    mathOperation.value = mathOperation.value.slice(0, -3);
+  } else {
+    mathOperation.value = mathOperation.value.slice(0, -1);
+  }
 };
 
+const handleCount = () => {
+  mathOperation.value !== ""
+    ? (mathOperation.value = eval(mathOperation.value))
+    : null;
+};
+
+window.onerror = function (e) {
+  alert("Źle zapisane działanie matematyczne !");
+};
 for (let value of newValueBtn) {
   value.addEventListener("click", handleOperationChange);
 }
 
 resetBtn.addEventListener("click", handleReset);
+deletePrevNumBtn.addEventListener("click", handleDeletePrevNum);
 resultBtn.addEventListener("click", handleCount);
